9cfd8e9583502a2999f1771f397d604589b77218
cew216 <cew216@skbl-422-012.ph.ic.ac.uk>
2018-11-05 16:31:27 +0000

    V to Temp conversion

--- RunMe.asm
+++ RunMe.asm
@@ -8,6 +8,8 @@
 acs0	    udata_acs		    ; reserve data space in access ram
 counter	    res 1		    ; reserve one byte for a counter variable
 delay_count res 1		    ; reserve one byte for counter in the delay routine
+offset	    res 1		    ; reserve one byte for the offset in the V-T conversion
+temp	    res 1		    ; reserve one byte for tempory values
 
 tables	    udata	0x400		    ; reserve data anywhere in RAM (here at 0x400)
 myArray	    res 0x80		    ; reserve 128 bytes for message data
@@ -19,13 +21,15 @@ main	code
 	; ******* Programme FLASH read Setup Code ***********************
 setup	bcf	EECON1, CFGS	    ; point to Flash program memory  
 	bsf	EECON1, EEPGD	    ; access Flash program memory
-	call	UART_Setup	    ; setup UART
+	;call	UART_Setup	    ; setup UART
 	call	LCD_Setup	    ; setup LCD
 	call	ADC_Setup	    ; setup ADC
 	goto	start
 	
 	; ******* Main programme ****************************************
-start 	
+start 	movlw	0x0D
+	movwf	offset
+	bra	measure_loop
 	
 measure_loop
 	call	ADC_Read	    ;get out a hex value for voltage
@@ -40,10 +44,18 @@ measure_loop
 	goto	measure_loop	    ; goto current line in code
 	
 Algorithm			    ;follows procedure as outlined in lec9
-	movf	ADRESH, W
-	movwf	numbH
 	movf	ADRESL, W
+	movwf	temp
+	movf	offset, W
+	subwf	temp, f
+	movf	temp, W
 	movwf	numbL
+	movf	ADRESH, W
+	movwf	temp
+	movlw	0x0
+	subwfb	temp,f
+	movf	temp, W
+	movwf	numbH
 	call	M_16x16
 	call	M_SelectHigh
 	call	LCD_Send_Byte_D
@@ -56,6 +68,8 @@ Algorithm			    ;follows procedure as outlined in lec9
 	call	M_SelectHigh
 	call	LCD_Send_Byte_D
 	call	M_Move
+	movlw	'.'
+	call	LCD_Send_Byte_D
 	call	M_8x24
 	call	M_SelectHigh
 	call	LCD_Send_Byte_D
