1cc24f65c400805824c6ec655ef633d55ae86442
cew216 <cew216@skbl-422-012.ph.ic.ac.uk>
2018-11-06 15:52:47 +0000

    V to Temp conversion commented.

---
+++ Heating_control.asm
@@ -0,0 +1,8 @@
+#include p18f87k22.inc
+    
+    ;Need to build a comparison routine
+    ;Need to build a rotuine that turns the heater on.
+    ;This routine needs to  output '0000 0001' to PORTJ.
+    
+    ;Need to build a routine that tunrs the heater off.
+    ;This routine needs to  output '0000 0001' to PORTJ.
