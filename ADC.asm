331bb1f8a451d765cef8df11755df25507aee0cd
cew216 <cew216@skbl-422-012.ph.ic.ac.uk>
2018-11-08 16:20:40 +0000

    Basics - completed

--- ADC.asm
+++ ADC.asm
@@ -1,6 +1,6 @@
 #include p18f87k22.inc
 
-    global	ADC_Setup, ADC_Read, M_Table
+    global	ADC_Setup, ADC_Read
     extern	delay, LCD_Write_Hex, tmpval
 
 		
@@ -24,49 +24,4 @@ adc_loop
     bra	    adc_loop
     return
     
-M_Table			    ;lookup table (basically)
-    movlb   6		    ;use Bank 6
-    lfsr    FSR2, 0x680	    ;start at 0x680 address in Bank 6
-    movlw   '1'		    ;ascii characters into files in Bank 6
-    movwf   tmpval
-    movlw   0x1
-    movff   tmpval,PLUSW2
-    movlw   '2'
-    movwf   tmpval
-    movlw   0x2
-    movff   tmpval,PLUSW2
-    movlw   '3'
-    movwf   tmpval
-    movlw   0x3
-    movff   tmpval,PLUSW2
-    movlw   '4'
-    movwf   tmpval
-    movlw   0x4
-    movff   tmpval,PLUSW2
-    movlw   '5'
-    movwf   tmpval
-    movlw   0x5 
-    movff   tmpval,PLUSW2
-    movlw   '6'
-    movwf   tmpval
-    movlw   0x6
-    movff   tmpval,PLUSW2
-    movlw   '7'
-    movwf   tmpval
-    movlw   0x7
-    movff   tmpval,PLUSW2
-    movlw   '8'
-    movwf   tmpval
-    movlw   0x8
-    movff   tmpval,PLUSW2
-    movlw   '9'
-    movwf   tmpval
-    movlw   0x9
-    movff   tmpval,PLUSW2
-    movlw   '0'
-    movwf   tmpval
-    movlw   0x0
-    movff   tmpval,PLUSW2
-    return
-
     end
