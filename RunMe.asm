1aeff56a281ab0b3eb5fc18bc418f267d24b5e04
cew216 <cew216@skbl-422-012.ph.ic.ac.uk>
2018-11-09 12:22:11 +0000

    Timers - basics

--- RunMe.asm
+++ RunMe.asm
@@ -1,7 +1,7 @@
 	#include p18f87k22.inc
 
-	extern  LCD_Setup, LCD_Clear, LCD_Send_Byte_D, LCD_delay_ms ; external LCD subroutines
-	extern  ADC_Setup, ADC_Read		    ; external ADC routines
+	extern  LCD_Setup, LCD_Clear, LCD_Send_Byte_D, LCD_delay_ms 
+	extern  ADC_Setup, ADC_Read		    
 	extern	M_16x16, M_8x24, numbL, numbH, numbU, M_SelectHigh, M_Move
 	extern	FDLP, T_in_d_h, hundreds, tens, units
 	extern	LCD_Alg, Keys_Translator, LookUp_d_h, M_Table
@@ -17,8 +17,8 @@ T_CrntH	    res 1		    ;reserved for the current voltage readout off the LM35
 TimerCount  res 1		    ;number of seconds passed
 DataCount   res 1		    ;number of seconds between data readouts
 
-tables	    udata	0x400		    ; reserve data anywhere in RAM (here at 0x400)
-myArray	    res 0x80		    ; reserve 128 bytes for message data
+tables	    udata	0x400	    ; reserve data anywhere in RAM (here at 0x400)
+myArray	    res		0x80	    ; reserve 128 bytes for message data
 
 rst	code	0		    ; reset vector
 	goto	setup
@@ -30,28 +30,28 @@ setup	bcf	EECON1, CFGS	    ; point to Flash program memory
 	call	UART_Setup	    ; setup UART
 	call	LCD_Setup	    ; setup LCD
 	call	ADC_Setup	    ; setup ADC
-				; sets up our 3 look up tables
+	; ***sets up our 3 look up tables***
+	call	LookUp_d_h	    ; using FSR0			
 	call	Keys_Translator	    ; using FSR1
-	call	LookUp_d_h	    ; using FSR0
 	call	M_Table		    ; using FSR2
-	movlw	0x0a
-	movwf	DataCount
 	goto	start
 	
 	; ******* Main programme ****************************************
-start 	movlw	0x0D
+start 	movlw	0x0a		    ; define time in sec between data readings
+	movwf	DataCount
+	movlw	0x0D		    ; callibrates between mV and T readings
 	movwf	offset
 	clrf	PORTJ, ACCESS	    ;cleared for use later with powering heater
-	movlw	0x0A
+	movlw	0x0A		    ; for comparison loops later (in T_in_d_h)
 	movwf	hundreds
 	movwf	tens
 	movwf	units
-	call	T_in_d_h
-	call	SecondTimer
+	call	T_in_d_h	    ; converts Temp in decimal to hex voltage
+	call	SecondTimer	    ; sets up timer/interrupts for data readings
 	bra	measure_loop
 	
 measure_loop
-	call	ADC_Read	    ;get out a hex value for voltage
+	call	ADC_Read	    ; get out a hex value for voltage
 	movlw	.250
 	call	LCD_delay_ms
 	movlw	.250
@@ -59,9 +59,9 @@ measure_loop
 	movlw	.250
 	call	LCD_delay_ms
 	call	LCD_Clear
-	call	LCD_Alg		    ;convert hex to decimal and output to LCD
-	call	FDLP
-	goto	measure_loop	    ; goto current line in code
+	call	LCD_Alg		    ; converts hex to decimal and output to LCD
+	call	FDLP		    ; determines in heater should be on/off
+	goto	measure_loop	    ; holds the system in this loop
 		
 
 	; a delay subroutine if you need one, times around loop in delay_count
