bf6a6f590d9031891d11d5e8067789e31a1c4009
cew216 <cew216@skbl-422-012.ph.ic.ac.uk>
2018-11-12 15:40:30 +0000

    Routine Select - A and C sorted

--- RunMe.asm
+++ RunMe.asm
@@ -1,21 +1,27 @@
 	#include p18f87k22.inc
 
 	extern  LCD_Setup, LCD_Clear, LCD_Send_Byte_D, LCD_delay_ms 
-	extern  ADC_Setup, ADC_Read		    
+	extern  ADC_Setup, ADC_Read, Keypad, FDLP_Time, FDLP_Temp	    
 	extern	M_16x16, M_8x24, numbL, numbH, numbU, M_SelectHigh, M_Move
-	extern	FDLP, T_in_d_h, hundreds, tens, units
+	extern	T_in_d_h, hundreds, tens, units
 	extern	LCD_Alg, Keys_Translator, LookUp_d_h, M_Table, TempIn_Alg
 	extern	SecondTimer, UART_Transmit_Byte, UART_Setup, Time_alg
-	global	delay, T_CrntL, T_CrntH, measure_loop, offset, TimerCount, DataCount
+	global	delay, T_CrntL, T_CrntH, offset, TimerCount, DataCount, TimeL, TimeH
 	
 acs0	    udata_acs		    ; reserve data space in access ram
 counter	    res 1		    ; reserve one byte for a counter variable
 delay_count res 1		    ; reserve one byte for counter in the delay routine
 offset	    res 1		    ; reserve one byte for the offset in the V-T conversion
 T_CrntL	    res 1
-T_CrntH	    res 1		    ;reserved for the current voltage readout off the LM35
-TimerCount  res 1		    ;number of seconds passed
-DataCount   res 1		    ;number of seconds between data readouts
+T_CrntH	    res 1		    ; reserved for the current voltage readout off the LM35
+TimerCount  res 1		    ; number of seconds passed between data readouts
+DataCount   res 1		    ; number of seconds between data readouts
+TimeL	    res 1		    ; 2 bytes for the 'real time'
+TimeH	    res 1
+Aascii	    res 1		    ; for routine select (A, B or C)
+Bascii	    res 1
+Cascii	    res 1
+Ascii	    res 1		    ; holds the ascii in routine select
 
 tables	    udata	0x400	    ; reserve data anywhere in RAM (here at 0x400)
 myArray	    res		0x80	    ; reserve 128 bytes for message data
@@ -34,6 +40,16 @@ setup	bcf	EECON1, CFGS	    ; point to Flash program memory
 	call	LookUp_d_h	    ; using FSR0			
 	call	Keys_Translator	    ; using FSR1
 	call	M_Table		    ; using FSR2
+	clrf	TimerCount	    ; reset the timers
+	clrf	DataCount
+	clrf	TimeL
+	clrf	TimeH
+	movlw	0x41
+	movwf	Aascii
+	movlw	0x42
+	movwf	Bascii
+	movlw	0x43
+	movwf	Cascii
 	goto	start
 	
 	; ******* Main programme ****************************************
@@ -47,12 +63,31 @@ start 	movlw	0x0A		    ; define time in sec between data readings
 	movwf	tens
 	movwf	units
 	call	T_in_d_h	    ; converts Temp in decimal to hex voltage
-	call	Time_alg
-	call	TempIn_Alg
+	movlw	.250
+	call	LCD_delay_ms
+	movlw	.250
+	call	LCD_delay_ms
+	movlw	.250
+	call	LCD_delay_ms
+	call	LCD_Clear
 	call	SecondTimer	    ; sets up timer/interrupts for data readings
-	bra	measure_loop
+	bra	RoutineSelect
 	
-measure_loop
+RoutineSelect
+	call	Keypad		    ; puts ascii value for A, B or C onto W
+	movwf	Ascii
+	call	LCD_Send_Byte_D
+	movf	Ascii, W
+	cpfseq	Aascii		    ; routine A select?
+	bra	Continue
+	bra	TempLoop
+Continue  
+   	cpfseq	Bascii		    ; routine B or C select? 
+	bra	TimeLoop
+	bra	PowerLoop
+	
+TempLoop			    ; Routine A: input temp vs current temp
+	call	TempIn_Alg
 	call	ADC_Read	    ; get out a hex value for voltage
 	movlw	.250
 	call	LCD_delay_ms
@@ -62,10 +97,34 @@ measure_loop
 	call	LCD_delay_ms
 	call	LCD_Clear
 	call	LCD_Alg		    ; converts hex to decimal and output to LCD
-	call	FDLP		    ; determines in heater should be on/off
-	goto	measure_loop	    ; holds the system in this loop
+	call	FDLP_Temp	    ; determines in heater should be on/off
+	goto	TempLoop	    ; holds the system in this loop
 		
-
+PowerLoop			    ; Routine B: power calculation controls heat
+	movlw	.250
+	call	LCD_delay_ms
+	movlw	.250
+	call	LCD_delay_ms
+	movlw	.250
+	call	LCD_delay_ms
+	call	LCD_Clear
+	bra	PowerLoop
+	
+TimeLoop			    ; Routine C: input time vs current time
+	call	Time_alg
+	call	ADC_Read	    ; get out a hex value for voltage
+	movlw	.250
+	call	LCD_delay_ms
+	movlw	.250
+	call	LCD_delay_ms
+	movlw	.250
+	call	LCD_delay_ms
+	call	LCD_Clear
+	call	LCD_Alg		    ; converts hex to decimal and output to LCD
+	call	FDLP_Time
+	goto	TimeLoop
+	
+	
 	; a delay subroutine if you need one, times around loop in delay_count
 delay	decfsz	delay_count	; decrement until zero
 	bra delay
