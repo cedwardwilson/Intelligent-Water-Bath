36e2c985249fcc2ddb156219e03a14d59a23a7ff
cew216 <cew216@skbl-422-012.ph.ic.ac.uk>
2018-11-20 16:51:02 +0000

    Tidy Up - 3

--- UART.asm
+++ UART.asm
@@ -1,24 +1,29 @@
 #include p18f87k22.inc
+; File Overview:
+; Contains UART setup and transmit functions to allow data to be sent to the
+; serial port terminal
 
+    ; Global routines/variables
     global  UART_Setup, UART_Transmit_Message, UART_Transmit_Byte
 
-acs0    udata_acs	    ; named variables in access ram
-UART_counter res 1   ; reserve 1 byte for variable UART_counter	;res 1
+    ; Named variables in Access Ram
+acs0    udata_acs	  
+UART_counter res 1		; Reserve 1 byte for variable UART_counter
 
 UART    code
     
 UART_Setup
-    bsf	    RCSTA1, SPEN    ; enable
-    bcf	    TXSTA1, SYNC    ; synchronous
-    bcf	    TXSTA1, BRGH    ; slow speed
-    bsf	    TXSTA1, TXEN    ; enable transmit
-    bcf	    BAUDCON1, BRG16 ; 8-bit generator only
-    movlw   .103	    ; gives 9600 Baud rate (actually 9615)
+    bsf	    RCSTA1, SPEN	; Enable
+    bcf	    TXSTA1, SYNC	; Synchronous
+    bcf	    TXSTA1, BRGH	; Slow speed
+    bsf	    TXSTA1, TXEN	; Enable transmit
+    bcf	    BAUDCON1, BRG16	; 8-bit generator only
+    movlw   .103		; Gives 9600 Baud rate (actually 9615)
     movwf   SPBRG1
-    bsf	    TRISC, TX1	    ; TX1 pin as output
+    bsf	    TRISC, TX1		; TX1 pin as output
     return
-
-UART_Transmit_Message	    ; Message stored at FSR2, length stored in W
+    
+UART_Transmit_Message		; Message stored at FSR2???, length stored in W
     movwf   UART_counter
 UART_Loop_message
     movf    POSTINC2, W
@@ -27,9 +32,13 @@ UART_Loop_message
     bra	    UART_Loop_message
     return
 
-UART_Transmit_Byte	    ; Transmits byte stored in W
-    btfss   PIR1,TX1IF	    ; TX1IF is set when TXREG1 is empty
-    bra	    UART_Transmit_Byte
+    ; UART_Transmit_Byte:
+	; Transmits a byte of information to the UART (which reads out in the
+	; serial port terminal)
+	;				- requires W (this is what is sent)
+UART_Transmit_Byte	   
+    btfss   PIR1,TX1IF		; TX1IF is set when TXREG1 is empty
+    bra	    UART_Transmit_Byte	; Sits in this loops until byte is sent
     movwf   TXREG1
     return
 
