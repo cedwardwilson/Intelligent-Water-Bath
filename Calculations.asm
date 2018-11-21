c6d234309716364b7a3ceae46b197df41b894c6f
cew216 <cew216@skbl-422-012.ph.ic.ac.uk>
2018-11-21 17:10:47 +0000

    Tidy Up - 4

--- Calculations.asm
+++ Calculations.asm
@@ -1,36 +1,44 @@
 #include p18f87k22.inc
+; File Overview:
+; Contains routines for doing 16 by 16 bit and 8 by 24 bit multiplication
     
-    global	M_16x16, M_8x24, numbH, numbL, numbU, M_SelectHigh, M_Move, ru
-    extern	delay, LCD_delay_ms, M_Table
+	; External and global routines/variables
+	global	    M_16x16, M_8x24, M_SelectHigh, M_Move
+	global	    numbH, numbL, numbU, ru
+	extern	    delay
     
-acs0    udata_acs 
-;below we resevre all the variables that are relevant to calculations
-numa		res 1
-numbH		res 1
-numbL		res 1
-numbU		res 1
-numcH		res 1
-numcL		res 1
-tmpnumLL	res 1
-tmpnumLH	res 1
-tmpnumLU	res 1
-tmpnumHL	res 1
-tmpnumHH	res 1
-tmpnumHU	res 1
-tmpnumUL	res 1
-tmpnumUH	res 1
-tmpnumUU	res 1
-rb		res 1	   
-rl		res 1	    
-rh		res 1	    
-ru		res 1
-rt		res 1
-purse		res 1
+	; Named variables in Access Ram
+acs0		udata_acs
+numa		res 1		; Variable storage bytes
+numbH		res 1		; "
+numbL		res 1		; "    
+numbU		res 1		; "  
+numcH		res 1		; "  
+numcL		res 1		; "  
+tmpnumLL	res 1		; "  
+tmpnumLH	res 1		; "  
+tmpnumLU	res 1		; "  
+tmpnumHL	res 1		; "  
+tmpnumHH	res 1		; "  
+tmpnumHU	res 1		; "  
+tmpnumUL	res 1		; "  
+tmpnumUH	res 1		; "  
+tmpnumUU	res 1		; "  
+rb		res 1		; "  	   
+rl		res 1		; "  	    
+rh		res 1		; "  	    
+ru		res 1		; "  
+rt		res 1		; "  
+purse		res 1		; "  
 		
 Calculations	code
 	
-M_16x16				;multiplies a 16-bit by a 16-bit 
-    call    M_16Setup		;and stores output as 3 bytes (rb, rh, ru)
+	; M_16x16:
+	; Multiplies two 16 bit numbers together
+	;			- requires numbL, numbH ( = 16 bit number)
+	;			- puts result into rb, rl, rh, ru
+M_16x16				 
+    call    M_16Setup		
     movff   numcL, numa
     call    M_CalculateL
     movff   numcH, numa
@@ -38,41 +46,46 @@ M_16x16				;multiplies a 16-bit by a 16-bit
     call    M_addHL16
     return
   
-M_8x24				;multiplies an 8-bit by a 24-bit
-    call    M_24Setup		;and stores output as 4 bytes (rb, rh, ru, rt)
+	; M_8x24:
+	; Multiplies an 8 bit and a 24 bit number together
+	;			- requires numbL, numbH, numbU (= 24 bit number)
+	;			- puts result into rb, rl, rh, ru, rt
+M_8x24				
+    call    M_24Setup		
     call    M_CalculateL
     call    M_CalculateU
     call    M_addHL24
     return
   
-M_16Setup			;must run before a 16-bit by 16-bit multiply
-    ;call    M_Table
-    clrf    tmpnumLL		;ensure the temporary memeory locations clear
+M_16Setup			
+    clrf    tmpnumLL		; Ensures temporary files are clear
     clrf    tmpnumLH
     clrf    tmpnumLU
     clrf    tmpnumHL
     clrf    tmpnumHH
     clrf    tmpnumHU	
-    movlw   0x8A		;k value stored as 2 bytes
-    movwf   numcL
+    movlw   0x8A		; This forces one of 16 bit numbers to always
+    movwf   numcL		; be 0x418A ( = 16778, for ADC)
     movlw   0x41
     movwf   numcH
     return
   
-M_24Setup			;must run before 1n 8-bit by 24-bit multiply
-    ;call    M_Table
-    clrf    tmpnumLL
+M_24Setup			
+    clrf    tmpnumLL		; Ensures temporary files are clear
     clrf    tmpnumLH
     clrf    tmpnumLU
     clrf    tmpnumHL
     clrf    tmpnumHH
     clrf    tmpnumHU	
-    movlw   0x0A
+    movlw   0x0A		; This forces 8 bit number to be 10 (0x0A)
     movwf   numa
     return
-  
-M_CalculateL		  ;calculating low byte of result from multiplication
-			  ;involving adding with carries
+	
+	; M_CalculateL:
+	; Calculates the low byte of a multiplication 
+	;			- requires numa, numbL, numbH
+	;			- sets purse, tmpnumLL, tmpnumLH, tmpnumLU
+M_CalculateL			
     movf    numa, W
     mulwf   numbL
     movff   PRODL, tmpnumLL
@@ -84,9 +97,12 @@ M_CalculateL		  ;calculating low byte of result from multiplication
     movf    PRODH, W
     addwfc  tmpnumLU, f
     return
-  
-M_CalculateH		   ;calculating high byte of result from multiplication
-			   ;involving adding with carries
+	
+	; M_CalculateH:
+	; Calculates the high byte of a multiplication 
+	;			- requires numa, numbL, numbH
+	;			- sets purse, tmpnumHL, tmpnumHH, tmpnumHU
+M_CalculateH		   
     movf    numa, W	   
     mulwf   numbL
     movff   PRODL, tmpnumHL
@@ -99,15 +115,23 @@ M_CalculateH		   ;calculating high byte of result from multiplication
     addwfc  tmpnumHU, f
     return
   
-M_CalculateU		   ;calculating upper byte of result from multiplication
-			   ;involving adding with carries
+	; M_CalculateU:
+	; Calculates the upper byte of a multiplication 
+	;			- requires numa, numbU
+	;			- sets tmpnumUL, tmpnumUH
+M_CalculateU		  
     movf    numa, W
     mulwf   numbU
     movff   PRODL, tmpnumUL
     movff   PRODH, tmpnumUH
     return
- 
-M_addHL16			;addition to give final 16x16 bit calculation
+    
+	; M_addHL16:
+	; Finishes the 16x16 bit multiplication by adding the relevant bits 
+	;			- requires  tmpnumLL, tmpnumLH, tmpnumLU,
+	;			tmpnumHL, tmpnumHH, tmpnumHU
+	;			- sets rb, rl, rh, ru
+M_addHL16			
     movff   tmpnumLL, rb
     movf    tmpnumLH, w
     addwf   tmpnumHL, w
@@ -119,8 +143,13 @@ M_addHL16			;addition to give final 16x16 bit calculation
     addwfc  tmpnumHU, f
     movff   tmpnumHU, ru
     return
-  
-M_addHL24			;addition to give final 8x24 bit calculation
+    
+	; M_addHL24:
+	; Finishes the 8x24 bit multiplication by adding the relevant bits 
+	;			- requires  tmpnumLL, tmpnumLH, tmpnumLU,
+	;			tmpnumUL, tmpnumUH
+	;			- sets rb, rl, rh, ru
+M_addHL24			
     movff   tmpnumLL, rb
     movff   tmpnumLH, rl
     movf    tmpnumLU, W
@@ -130,16 +159,24 @@ M_addHL24			;addition to give final 8x24 bit calculation
     addwfc  tmpnumUH, f
     movff   tmpnumUH, ru
     return
- 
-M_SelectHigh	    		   ;grabs top byte of result of latest multiply
-    movf    ru, W, ACCESS	   ;ru is upper byte of the result  
-    movff   PLUSW2, purse	   ;puts value at fsr2+w into purse  	
+    
+	; M_SelectHigh:
+	; Moves the ascii character equivalent to the top byte of the latest
+	;  multiplication into W
+	;			    - requires ru
+M_SelectHigh	    		   
+    movf    ru, W, ACCESS	     
+    movff   PLUSW2, purse	   ; Uses M_Table lookup table (at FSR2) 	
     call    delay			
     movf    purse, W
     return
-
-M_Move				    ;moves latest result into correct bytes 
-    movff   rb, numbL		    ;so it can be used again in following calc
+	
+	; M_Move:
+	; Shifts the latest multiplication result up one byte
+	;			    - requires rb, rl, rh
+	;			    - sets numbL, numbH, numbU
+M_Move				    
+    movff   rb, numbL		    
     movff   rl, numbH
     movff   rh, numbU
     return
